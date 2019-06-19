005fdf5ea78c7a5a158d51f15c6280a8861391f0
SK-CSE <saurabh.cse04@gmail.com>
2019-06-19 15:15:58 +0530

    updated

--- index.js
+++ index.js
@@ -21,58 +21,58 @@ let mongoCon;
 // db connection
 const MongoURI = process.env.MONGO_URL || config.MongoURI;
 mongoose.createConnection(MongoURI)
-.then(conn =>{
+  .then(conn => {
     mongoCon = conn.model('Scraperdata', scraperSchema);
     // console.log('==xx ',mongoCon);
-})
-.catch(err =>{
+  })
+  .catch(err => {
     console.error('err', err);
     process.exit(1);
-})
+  })
 
 let scrap = async function (req, res) {
-    try {
-        let arr = [];
-        let uri = 'https://medium.com/';
+  try {
+    let arr = [];
+    let uri = 'https://medium.com/';
 
-        let html = await request.get(uri);
-        let $ = cheerio.load(html.toString());
-        // console.log($);
-        // filter url and its params from all links 
-        $("a").each((i, link) => {
-          let allHref = URL.parse($(link).attr("href"), true);
-          // console.log(allHref);
-          let qArr = Object.keys(allHref['query']);
-          // console.log(qArr);
-          let url = allHref['href'].split('?')[0];
-          // console.log(url, qArr);
-          arr.push({ url, qArr});
-        });
-        // console.log(arr);
+    let html = await request.get(uri);
+    let $ = cheerio.load(html.toString());
+    // console.log($);
+    // filter url and its params from all links 
+    $("a").each((i, link) => {
+      let allHref = URL.parse($(link).attr("href"), true);
+      // console.log(allHref);
+      let qArr = Object.keys(allHref['query']);
+      // console.log(qArr);
+      let url = allHref['href'].split('?')[0];
+      // console.log(url, qArr);
+      arr.push({ url, qArr });
+    });
+    // console.log(arr);
 
-        let urlsArr = arr.map(x => x['url']);
-        // console.log(urlsArr);
-        let urlsSet = new Set(urlsArr);
-        // console.log(urlsSet);
+    let urlsArr = arr.map(x => x['url']);
+    // console.log(urlsArr);
+    let urlsSet = new Set(urlsArr);
+    // console.log(urlsSet);
 
-        // filter unique url and its count
-        let arrObj = [...urlsSet].map(url => {
-           return {
-            url: url, 
-            totalRef: arr.filter(e => e['url'] === url).length, 
-            params: arr.find(e => e['url'] === url).qArr,
-           } 
-        });
+    // filter unique url and its count
+    let arrObj = [...urlsSet].map(url => {
+      return {
+        url: url,
+        totalRef: arr.filter(e => e['url'] === url).length,
+        params: arr.find(e => e['url'] === url).qArr,
+      }
+    });
 
-        // console.log(arrObj);
+    // console.log(arrObj);
 
-        let mongoRes = await mongoCon.collection.insertMany(arrObj);
-        // console.log('==========',mongoRes);
-        res.status(200).jsonp({"total_URL_count": mongoRes['result']['n'],"scrapData":mongoRes['ops']});
-    } catch(err){
-        console.error(err);
-        res.status(500).jsonp({ 'msg': 'internal server error', 'err' : err });    
-    }
+    let mongoRes = await mongoCon.collection.insertMany(arrObj);
+    // console.log('==========',mongoRes);
+    res.status(200).jsonp({ "total_URL_count": mongoRes['result']['n'], "scrapData": mongoRes['ops'] });
+  } catch (err) {
+    console.error(err);
+    res.status(500).jsonp({ 'msg': 'internal server error', 'err': err });
+  }
 };
 
 
@@ -81,10 +81,10 @@ app.get('/', scrap);
 
 // Server starts.
 app.listen(app.get("port"), () => {
-    console.log(
-      "  App is running at port %d",
-      app.get("port")
-    );
-    console.log("  Press CTRL-C to stop\n");
-  });
+  console.log(
+    "  App is running at port %d",
+    app.get("port")
+  );
+  console.log("  Press CTRL-C to stop\n");
+});
 
